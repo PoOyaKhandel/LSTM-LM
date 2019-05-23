9439317c9534373f62d6c5b0bf5e77e1fb0ed722
po_oya <pooyakhandel@gmail.com>
2019-05-23 14:41:38 +0430

    very basic dev of preprocessing

--- main.py
+++ main.py
@@ -1,9 +1,60 @@
+import prerpocess_corpus
+from numpy import array
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from pickle import dump
+
 
 # Pre-process corpus(removing extra files, removing out of range words, ...
+source_corpus_path = "MirasText_sample.txt"
+clean_corpus_path = "clean_corpus.txt"
+source_text = prerpocess_corpus.load_doc(source_corpus_path)
+clean_sequences = prerpocess_corpus.clean_doc(source_text)
+prerpocess_corpus.save_doc(clean_sequences, clean_corpus_path)
+
+doc = prerpocess_corpus.load_doc(clean_corpus_path)
+lines = doc.split("\n")
+
 
 
 # Training and Model definition
+tokenizer = Tokenizer()
+print(len(lines[0]), len(lines[1]), len(lines[2]))
 
-# Testing the Model
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+# vocabulary size
+vocab_size = len(tokenizer.word_index) + 1
+print(len(sequences[0]), len(sequences[1]), len(sequences[50]))
 
+# separate into input and output
+sequences = array(sequences)
+# x = pad_sequences(sequences, padding='post')
+# print(x)
+X, y = sequences[:-1], sequences[-1]
+y = to_categorical(y, num_classes=vocab_size)
+seq_length = X.shape[1]
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 50, input_length=seq_length))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit model
+model.fit(X, y, batch_size=128, epochs=100)
+
+# save the model to file
+model.save('model.h5')
+# save the tokenizer
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
 
--- prerpocess_corpus.py
+++ prerpocess_corpus.py
@@ -2,74 +2,138 @@
 This module will pre-process Miras corpus, removes extra information, keeps only content of each sample and also
 removes non-persian characters
 """
+import stanfordnlp
+import re
+
+class ProcessCorpus:
+    """
+
+    """
+    def __init__(self, fname):
+        self.nlp_pipeline = stanfordnlp.Pipeline(processors='tokenize', lang='fa')
+        all_text = self.__load_doc(fname)
+        self.useful_content = self.__remove_extra_info(all_text)
+        # self.write_file(relavant_text, 'check.txt')
+
+    @staticmethod
+    def __load_doc(filename):
+        file = open(filename, 'r')
+        text = file.read()
+        file.close()
+        return text
+
+    @staticmethod
+    def __remove_extra_info(text):
+        lines = text.split("\n")
+        final_tokens = []
+        for line in lines:
+            tokens = line.split('***')
+            tokens = tokens[0]
+            # removing extra spaces
+            tokens = tokens.strip()
+            final_tokens.append(tokens)
+            # doc = self.nlp_pipeline(tokens)
+            # for sent in doc.sentences:
+            #     final_tokens.append(' '.join(sent.words))
+
+        final_tokens = '\n'.join(final_tokens[:4])
+        return final_tokens
+
+    @staticmethod
+    def __write_file(text, fname):
+        # print(text)
+        file = open(fname, 'w')
+        file.write(text)
+
+    @staticmethod
+    def __remove_chars(sentence):
+        return re.sub(
+            '[^ \u0622\u0627\u0628\u067E\u062A-\u062C\u0686\u062D-\u0632\u0698\u0633'
+            '-\u063A\u0641\u0642\u06A9\u06AF\u0644-\u0648\u06CC\u200c]',
+            "", sentence)
+
+    @staticmethod
+    def __sentence_processing():
+        pass
+
+    def content_processing(self):
+        doc = self.nlp_pipeline(self.useful_content)
+        list_of_all_clen_sentences = []
+        for sent in doc.sentences:
+            sent_text = [a_word.text for a_word in sent.words]
+            sent_text = ' '.join(sent_text)
+            sent_after_removing_chars = self.__remove_chars(sent_text)
+            if not sent_after_removing_chars.isspace():
+                sent_after_removing_chars = sent_after_removing_chars.strip()
+                list_of_all_clen_sentences.append(sent_after_removing_chars)
+        print(list_of_all_clen_sentences[:2])
+        self.__write_file('\n'.join(list_of_all_clen_sentences), "clean_corpus.txt")
 
 
-def remove_non_persian(mixed_str):
-    mixed_str_words = mixed_str.split(' ')
-    empty = lambda x: x != ''
 
-    min_range = int("0x600", 0)
-    max_range = int("0x6FF", 0)
-    clean_str_words = []
-
-    for aWord in filter(empty, mixed_str_words):
-        if min_range < ord(aWord[0]) < max_range:
-            clean_str_words.append(aWord)
-    # clean_str = " ".join(clean_str_words)
-    return clean_str_words
-
-
-# load doc into memory
-def load_doc(filename):
-    # open the file as read only
-    file = open(filename, 'r')
-    # read all text
-    text = file.read()
-    # close the file
-    file.close()
-    return text
-
-
-def create_seq(tokens):
-    length = 49 + 1
-    sequences = []
-    for i in range(int(len(tokens)/length - 1)):
-        # select sequence of tokens
-        m = length * i
-        n = length * (i + 1) - 1
-        seq = tokens[m: n]
-        # convert into a line
-        line = ' '.join(seq)
-        # store
-        sequences.append(line)
-    return sequences
-
-
-# turn a doc into clean tokens
-def clean_doc(doc):
-    lines = doc.split("\n")
-    final_tokens = []
-    for line in lines:
-        tokens = line.split('***')
-        dirty_content = tokens[0]
-        clean_content = remove_non_persian(dirty_content)
-        if len(clean_content) != 0:
-            final_tokens.extend(clean_content)
-
-    sequences = create_seq(final_tokens)
-    return sequences
-
-
-# save tokens to file, one dialog per line
-def save_doc(lines, filename):
-    data = '\n'.join(lines)
-    file = open(filename, 'w')
-    file.write(data)
-    file.close()
+# def remove_non_persian(mixed_str):
+#     mixed_str_words = mixed_str.split(' ')
+#     empty = lambda x: x != ''
+#
+#     min_range = int("0x600", 0)
+#     max_range = int("0x6FF", 0)
+#     clean_str_words = []
+#
+#     for aWord in filter(empty, mixed_str_words):
+#         if min_range < ord(aWord[0]) < max_range:
+#             clean_str_words.append(aWord)
+#     # clean_str = " ".join(clean_str_words)
+#     return clean_str_words
+#
+#
+#
+#
+#
+# def create_seq(tokens):
+#     length = 50 + 1
+#     sequences = []
+#     for i in range(length, len(tokens)):
+#         # select sequence of tokens
+#         m = i - length
+#         n = i
+#         seq = tokens[m:n]
+#         # convert into a line
+#         c = len(seq)
+#         line = ' '.join(seq)
+#         # store
+#         a = len(line)
+#         b = tokens[n]
+#         sequences.append(line)
+#     return sequences
+#
+#
+# # turn a doc into clean tokens
+# def clean_doc(doc):
+#     lines = doc.split("\n")
+#     final_tokens = []
+#     for line in lines:
+#         tokens = line.split('***')
+#         dirty_content = tokens[0]
+#         clean_content = remove_non_persian(dirty_content)
+#         if len(clean_content) != 0:
+#             final_tokens.extend(clean_content)
+#
+#     sequences = create_seq(final_tokens)
+#     return sequences
+#
+#
+# # save tokens to file, one dialog per line
+# def save_doc(lines, filename):
+#     data = '\n'.join(lines)
+#     file = open(filename, 'w')
+#     file.write(data)
+#     file.close()
 
 
-if __name__ == "__main__":
-    f = '/home/po_oya/NLP Thesis/MirasText.txt'
-    f2 = "MirasText_sample.txt"
-    preprocess(f2, "processed_miras.txt")
+if __name__ == '__main__':
+    fname = "MirasText_sample.txt"
+    test = ProcessCorpus(fname)
+    test.content_processing()
+
+
 
