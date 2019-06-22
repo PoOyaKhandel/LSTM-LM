e97fae71eb57f434fad28a8ec0621ea992c8240b
po_oya <pooyakhandel@gmail.com>
2019-06-22 14:44:44 +0430

    second step of processing(sentence tokenization) is developed.

--- prerpocess_corpus.py
+++ prerpocess_corpus.py
@@ -7,6 +7,7 @@ import re
 from keras.preprocessing.text import Tokenizer
 import pandas as pd
 from random import shuffle
+from itertools import islice
 import numpy as np
 from multiprocessing import Process, Lock
 from time import time
@@ -23,10 +24,10 @@ def write_file(text, fname):
 class ProcessCorpus:
     """
     """
-    def __init__(self, path, n_files):
-        # self.nlp_pipeline = stanfordnlp.Pipeline(processors='tokenize', lang='fa')
-        self.path = path
+    def __init__(self, path, n_files, n_process):
+        self.base_file_path = path
         self.n_files = n_files
+        self.n_process = n_process
 
     def process_first_step(self):
         cwd = os.getcwd()
@@ -40,7 +41,7 @@ class ProcessCorpus:
         number_of_all_contents_after_first_step = 0
         dirty_sample = 0
         punctuation_set = set('\u200c\u060C\u061B\u061F\u002E\u0021 ')
-        with open(self.path, mode='r', encoding='utf8') as base_file:
+        with open(self.base_file_path, mode='r', encoding='utf8') as base_file:
             for line in base_file:
                 tokens = line.split('***')
                 tokens = tokens[0].strip()
@@ -54,30 +55,55 @@ class ProcessCorpus:
                 number_of_all_contents_after_first_step += 1
 
                 if number_of_all_contents_after_first_step % 100 == 0:
-                    print(number_of_all_contents_after_first_step," sample processed")
+                    print(number_of_all_contents_after_first_step, " sample processed")
 
         print("Total samples:", number_of_all_contents_after_first_step,
               "Clean samples:", number_of_all_contents_after_first_step - dirty_sample,
               "Dirty samples:", dirty_sample)
 
-    # @staticmethod
-    # def __load_doc(filename):
-    #     file = open(filename, 'r')
-    #     text = file.read()
-    #     file.close()
-    #     return text
-    # @staticmethod
-    # def __remove_extra_info(text):
-    #     lines = text.split("\n")
-    #     final_tokens = []
-    #     for line in lines:
-    #         tokens = line.split('***')
-    #         tokens = tokens[0]
-    #         # removing extra spaces
-    #         tokens = tokens.strip()
-    #         final_tokens.append(tokens)
-    #     # final_tokens = '\n'.join(final_tokens)
-    #     return final_tokens
+    def process_second_step(self):
+        cwd = os.getcwd()
+        step1_output_dir = cwd + '/step1_output/'
+        step2_output_dir = cwd + '/step2_output/'
+        if os.path.exists(step1_output_dir):
+            if os.path.exists(step2_output_dir):
+                raise NotImplementedError("second step processing exists!")
+            else:
+                os.mkdir(step2_output_dir)
+        else:
+            raise NotImplementedError("No first step processing exists!")
+
+        input_files = [step1_output_dir + "step" + str(i+1) + "process.txt" for i in range(self.n_files)]
+        output_files = [step2_output_dir + "step" + str(i + 1) + "process.txt" for i in range(self.n_files)]
+
+        all_process = []
+        print("Second Step Processing is Starting . . .")
+        t0 = time()
+        for i in range(self.n_process):
+            pr = Process(target=self.__sentence_processing, args=(input_files[i], output_files[i]))
+            all_process.append(pr)
+            pr.daemon = True
+            pr.start()
+
+        for a_pr in all_process:
+            a_pr.join()
+
+        print("########################################################################################################"
+              "HALF of INPUT FILES are NOW TOKENIZAED."
+              "#######################################################################################################")
+
+        for i in range(self.n_process):
+            pr = Process(target=self.__sentence_processing, args=(input_files[i+self.n_process], output_files[i]))
+            all_process.append(pr)
+            pr.daemon = True
+            pr.start()
+
+        for a_pr in all_process:
+            a_pr.join()
+
+        t1 = time()
+        print("Second Step Processing is Finised After: ", (t1-t0)/3600, " hour."
+              "Output files of this step are stored in: ", step1_output_dir, ".")
 
     @staticmethod
     def __remove_chars(sentence, mode='first_step'):
@@ -88,53 +114,54 @@ class ProcessCorpus:
                 '[^ \u0622\u0624\u0626\u0627\u0628\u062A-\u063A\u0641-\u064A\u06CC\u067E\u0686\u0698'
                 '\u06A9\u06AA\u06AF\u06BE\u06CC\u06D0\u200c\u060C\u061B\u061F\u002E\u0021]',
                 "", sentence)
-        else:
+        elif mode == 'second_step':
             return re.sub(
                 '[^ \u0622\u0624\u0626\u0627\u0628\u062A-\u063A\u0641-\u064A\u06CC\u067E\u0686\u0698'
                 '\u06A9\u06AA\u06AF\u06BE\u06CC\u06D0]',
                 "", sentence)
+        else:
+            raise NotImplementedError("Invalid mode of removing chars")
 
-    def __make_clean_content_list(self, a_content, lck):
-        lck.acquire()
-        # print("acontent:", a_content)
-        self.iteration += 1
-        print(self.iteration, " Contents processed!")
-        write_file(a_content, "clean_corpus.txt")
-        lck.release()
+    def __sentence_processing(self, input_file, output_file):
+        nlp_pipline = stanfordnlp.Pipeline(processors='tokenize', lang='fa')
+        with open(input_file, 'r') as inp_file:
+            print("Sentence tokenization procedure is Started for File: ", input_file,  ".")
 
-    def __sentence_processing(self, content, lck):
-        content = self.__remove_chars(content, mode=0)
-        # print("content:", content)
-        doc = self.nlp_pipeline(content)
-        list_of_sent_text = []
-        for sent in doc.sentences:
-            sent_text = [a_word.text for a_word in sent.words]
-            sent_text = ' '.join(sent_text)
-            sent_text = self.__remove_chars(sent_text)
-            if not sent_text.isspace():
-                sent_text = " ".join(sent_text.split())
-                list_of_sent_text.append(sent_text)
-        if len(list_of_sent_text) > 0:
-            self.__make_clean_content_list('\n'.join(list_of_sent_text) + '\n', lck)
+            batch_size = 100
+            data_processed = 0
+            finished = False
+            t_start = time()
+            a_text = list(islice(inp_file, data_processed, data_processed + batch_size - 1))
+            while not finished:
+                t_s_batch = time()
 
-    def content_processing(self):
-        all_process = []
-        lock = Lock()
-        t1 = time()
-        for content in self.content:
-            pr = Process(target=self.__sentence_processing, args=(content, lock))
-            all_process.append(pr)
-            pr.daemon = True
-            pr.start()
-            if len(all_process) == 4:
-                all_process[0].join()
-                all_process.remove(all_process[0])
+                data_processed += len(a_text)
+                doc = nlp_pipline('\n'.join(a_text))
+                list_of_sentence = []
 
-        t2 = time()
-        print("Sentence Tokenization Finished After", t2 - t1, "!")
-        print("Average time for a content", (t2-t1)/self.n_total_content)
-        print("Number of NonPersian Content:", self.n_total_content - self.iteration)
+                for sent in doc.sentences:
+                    sent_text = [a_word.text for a_word in sent.words]
+                    sent_text = ' '.join(sent_text)
+                    sent_text = self.__remove_chars(sent_text, mode='second_step')
+                    if not sent_text.isspace():
+                        sent_text = " ".join(sent_text.split())
+                        list_of_sentence.append(sent_text)
 
+                with open(output_file, 'a') as out_file:
+                    out_file.write('\n'.join(list_of_sentence) + '\n')
+                t_f_batch = time()
+
+                # if data_processed % 2000 == 0:
+                print(data_processed, "docs of ", output_file, " are processed. Average time for a batch "
+                                                               "per minute: ", (t_f_batch - t_s_batch) / 200)
+
+                a_text = list(islice(inp_file, data_processed, data_processed + batch_size - 1))
+                if len(a_text) == 0:
+                    finished = True
+
+            t_final = time()
+            print("Sentence tokenization for ", output_file, " is finished after", (t_final - t_start)/3600, "hour"
+                  "Average time per a tokenization: ", (t_final - t_start)/data_processed, " seconds.")
 
 
 class SentencePreparation:
@@ -214,12 +241,8 @@ class SentencePreparation:
 if __name__ == '__main__':
     BASE_FILE_PATH = "MirasText_sample.txt"
     n_files = 8
+    n_process = 4
 
-    pr_corpus = ProcessCorpus(BASE_FILE_PATH, n_files)
-    pr_corpus.process_first_step()
-    # print("useful_content")
-    # cleanning_unit.content_processing()
-
-
-
-
+    pr_corpus = ProcessCorpus(BASE_FILE_PATH, n_files, n_process)
+    # pr_corpus.process_first_step()
+    pr_corpus.process_second_step()
